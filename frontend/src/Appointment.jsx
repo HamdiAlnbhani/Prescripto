369aa147ae42feb06d2232bd6d823b110c7acb59
HamdiAlnbhani <hamdyalnbhany55@gmail.com>
2026-02-14 01:16:51 +0300

    first commit

---
+++ frontend/src/Appointment.jsx
@@ -0,0 +1,114 @@
+import React, {useState, useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from './context/APPContext'
+import { assets } from './assets/assets_frontend/assets'
+
+const Appointment = () => {
+  const {docId}=useParams()
+  const {doctors,currencySymbol }=useContext(AppContext)
+  const [docInfo,setDocInfo]=useState(null)
+  const [docSlots,setDocSlots]=useState([])
+  const[slotIndex,setSlotIndex]=useState(0)
+  const [slotTome,setSlotTime]=useState('')
+
+
+
+
+  const fetchDocInfo=async()=>{
+    const docInfo=doctors.find(doc=>doc._id===docId)
+    setDocInfo(docInfo)
+    
+  }
+
+  const getAvailableSlots= async()=>{
+    setDocSlots([])
+    //GETTING CURRENT DATE
+
+    let today=new Date()
+    
+    for(let i=0 ; i<7 ; i++){
+      //  GETTING DATA WITH INDEX
+      let currentDate=new Date(today)
+      currentDate.setDate(today.getDate()+i)
+
+      // SETTING END TIME OF THE DATE WITH INDEX
+      let endTime=new Date()
+      endTime.setDate(today.getDate()+i)
+      endTime.setHours(21,0,0,0)
+
+      // SETTING HOURS 
+      if(today.getDate()===currentDate.getDate()){
+        currentDate.setHours(currentDate.getHours()>10 ? currentDate.getHours()+1 : 10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 :  0)
+      }else{
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+      let timeSlots=[]
+       
+      while(currentDate<endTime){
+        let formattedTime=currentDate.toLocaleTimeString([],{hour:'2-digit',minute:'2-digit'})
+        // ADD SLOT TO ARRAY
+        timeSlots.push({
+          datetime:new Date(currentDate),
+          time:formattedTime
+        })
+
+        // INCREMENT CURRENT TIME BY 30 MINUTES
+        currentDate.setMinutes(currentDate.getMinutes()+30)
+      }
+      setDocSlots(prev=>([...prev,timeSlots]))
+    }
+
+  }
+
+  useEffect(()=>{
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    fetchDocInfo()
+
+  },[doctors,docId])
+
+  useEffect(()=>{
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    getAvailableSlots()
+  },[docInfo])
+
+  useEffect(()=>{
+     console.log(docSlots)
+  },[docSlots])
+  return docInfo && (
+    <div>
+      {/* DOCTOR DETAILS */}
+      <div className='flex flex-col  sm:flex-row gap-4'>
+        <div>
+          <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
+          
+        </div>
+        <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white ,x-2 sm:mx-0 mt-[-80px] sm:mt-0'>
+        {/* DOC INFO:NAME,DEGREE,EXPRIENCE */}
+        <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+          {docInfo.name} 
+          <img className='w-5 ' src={assets.verified_icon} alt="" />
+        </p>
+        <div className='flex items-center gap-2 tex-sm mt-1 text-gray-600'>
+          <p>{docInfo.degree}-{docInfo.speciality}</p>
+          <button className='py-0.5 px-2 border text-xs rounder-full '>{docInfo.experience}</button>
+        </div>
+         {/* DOCTOR ABOUT */}
+         <div>
+          <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>Abuot <img src={assets.info_icon} alt="" /></p>
+          <p className='ext-sm text-gray-500 max-w-[700PX]mt-1'>{docInfo.about}</p>
+         </div>
+         <p className=' text-gray-500 font-medium mt-4'>
+          Appointment fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span>
+         </p>
+       </div>
+        
+        
+      </div>
+        
+    </div>
+  )
+}
+
+export default Appointment
---
+++ frontend/src/components/Foteer.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { assets } from '../assets/assets_frontend/assets'
+
+const Foteer = () => {
+  return (
+    <div className='md:mx-10'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'> 
+            {/* LEFT SECTION */}
+            <div>
+                <img className='mb-5 w-40' src={assets.logo} alt="" />
+
+                <p className='w-full md:w-2/3 text-gray-600 leading-6'>lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Inpusm has been the industry`s standard dumy text ever since the 1500s,when an unknoe printer took a galley of type and scrambled it to make a type specimen book</p>
+
+            </div>
+
+             {/* CENTER SECTION */}
+            <div>
+                <p className='text-xl fount-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+
+             {/* RIGHT SECTION */}
+            <div>
+                <p  className='text-xl fount-medium mb-5'>GET IN TOUCH</p>
+                <ul  className='flex flex-col gap-2 text-gray-600'>
+                    <li>hamdialnbhani55@gmail.com</li>
+                    <li>hamdialnbhani55@gmail.com</li>
+                </ul>
+                
+            </div>
+        </div>
+        {/* COPYRIGHT TEXT */}
+        <div>
+            <hr />
+          <p className='py-5 text-sm text-center'>Copyright 2026@ prexcripto - All Right Reserved.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Foteer
