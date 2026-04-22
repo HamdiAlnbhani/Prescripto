916c9e5e7ef04c7daf0c49ef384c475f888771cf
HamdiAlnbhani <hamdyalnbhany55@gmail.com>
2026-04-22 22:32:21 +0300

    fix: correct import path for AppContext in MyAppointments component

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,5 +1,5 @@
 import React, { useContext,useState } from 'react'
-import {AppContext} from '../context/APPContext.jsx'
+import {AppContext} from '../context/AppContext.jsx'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { useEffect } from 'react'
